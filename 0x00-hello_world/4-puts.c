db5ef241f3f5517049b6dd8a0856ffe498eae8a3
viik01 <jayeolava.14@student.funaab.edu.ng>
2022-12-08 15:40:08 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - print a line of code using puts
-*
-* Return: 0
-*/
+ *main - print a line of code using puts
+ *
+ *Return: 0
+ */
 int main(void)
 {
-   puts("\"programming is like building a multilingual puzzle");
-return (0);
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
