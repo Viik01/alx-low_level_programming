815e8ea3924dcb987ffb942ee29714f115cab8f9
viik01 <jayeolava.14@student.funaab.edu.ng>
2022-12-08 15:33:36 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main -Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("size of a char: %lu byte(S)\n", (unsigned long) sizeof(a)); printf("size of an int: %lu byte(S) \n", (unsigned long) sizeof(b)); printf("size of a long int: %lu byte(S)\n", (unsigned long) sizeof(c)); printf("size of a long long int: %lu byte(S)\n", (unsigned long) sizeof(d)); printf("size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
+return (0);
+}
