35311aa710d87db0c4b1ed22a53f3fd9df4c3602
viik01 <jayeolava.14@student.funaab.edu.ng>
2022-12-14 18:40:19 -0800

    times table updated

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,24 +5,34 @@
  */
 void times_table(void)
 {
-	int num, mult, prod;
+	int x, y, z, u, d;
 
-	for (num = 0; num <= 9; num++)
+	for (x = 0; x <= 9; x++)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= 9; mult++)
+		for (y = 0; y <= 9; y++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			z = x * y;
+			if (z > 9)
+			{
+				u = z % 10;
+				d = (z - u) / 10;
 
-			prod = num * mult;
-
-			if (prod <= 9)
-				_putchar(' ');
-			_putchar((prod / 10) + '0');
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
+			}
+			else
+			{
+				if (y != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(z + '0');
+			}
 		}
 		_putchar('\n');
 	}
 }
-
