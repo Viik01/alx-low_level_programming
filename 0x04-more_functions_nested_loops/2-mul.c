1c76dc943d6ceb900b154827e75be6a6a927d92f
viik01 <jayeolava.14@student.funaab.edu.ng>
2022-12-15 11:17:59 -0800

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,4 +1,12 @@
 #include "main.h"
 
 /**
- *
+ * mul - muliply 2 num
+ * @a: first number
+ * @b: second number
+ * Return: result of multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
