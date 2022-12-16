8ff1dfb8acf623584addca4c384a93554c6122ad
viik01 <jayeolava.14@student.funaab.edu.ng>
2022-12-16 07:29:23 -0800

    main upd

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,11 @@ int main(void)
 	printf("infinte loop incoming :(\n");
 
 	i = 0;
-	/**
-	 * while (i < 10)
-	 * {
-	 * putchar(i);
-	 * }
-	 */
+
+       /*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 	printf("infinite loop avoided! \\o/\n");
 
 	return (0);
