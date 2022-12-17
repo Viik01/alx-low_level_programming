1948e571bf29e3a102a2fb930c249b7cd8f5bc66
viik01 <jayeolava.14@student.funaab.edu.ng>
2022-12-16 22:46:30 -0800

    upds

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,16 +4,14 @@
  * main - Prints the alphabet in lowercase.
  * Return: Always 0
  */
-
-int main(void)
+int main(void)
 {
-	char letter;
+	char letter;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter);
 
 	putchar('\n');
 
-	return (0);
-
+	return (0);
 }
