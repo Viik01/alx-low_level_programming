c6d38f7d8276827f44ceb0d2619a8eca817deb21
viik01 <jayeolava.14@student.funaab.edu.ng>
2023-01-13 06:52:58 -0800

    argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print its name and a new line
+ * @argc: argument count
+ * @argv: number of arrays
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
