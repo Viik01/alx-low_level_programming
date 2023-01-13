fb7ad28ec95abf50698846a4a058b5aef4669bc3
viik01 <jayeolava.14@student.funaab.edu.ng>
2023-01-13 09:19:11 -0800

    cmt

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,15 @@ int main(int argc, char **argv)
 {
 	int i, j;
 
-	i = atoi(argv[1]);
-	j = atoi(argv[2]);
-
-	printf("%d\n", i * j);
-	return (0);
-
 	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-}
 
+	i = atoi(argv[1]);
+	j = atoi(argv[2]);
+
+	printf("%d\n", i * j);
+	return (0);
+}
